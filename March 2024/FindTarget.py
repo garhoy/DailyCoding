e3dbd9d4e82d3493e3e42e9b823b5b76b2dea37c
garhoy <andergarciahoyberg@gmail.com>
2024-03-16 10:17:09 +0100

    Finished get Ranges

---
+++ March 2024/FindTarget.py
@@ -0,0 +1,38 @@
+'''
+Given a sorted array, A, with possibly duplicated elements, find the indices of the first and last occurrences of a target element, x.
+Return -1 if the target is not found.
+
+Example:
+Input: A = [1,3,3,5,7,8,9,9,9,15], target = 9
+Output: [6,8]
+
+Input: A = [100, 150, 150, 153], target = 150
+Output: [1,2]
+
+Input: A = [1,2,3,4,5,6,10], target = 9
+Output: [-1, -1]
+Here is a function signature example:
+'''
+
+class Solution: 
+    def getRange(self, arr, target):
+        flag = True
+        firstindex = -1
+        lastindex = -1
+
+        for i, num in enumerate(arr):
+
+            if num == target:
+                if flag:
+                    firstindex = i
+                    flag = False
+                
+                lastindex = i
+
+        return [firstindex,lastindex]
+  
+# Test program 
+arr = [1,2,3,4,5,6,10]
+x = 9
+print(Solution().getRange(arr, x))
+# [1, 4]
